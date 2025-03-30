39563cf09241f7a9da69639dffc92be80b6a1dd7
kinha28 <kinhasousa15@hotmail.com>
2025-03-29 23:25:02 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+// Função que mostra o alerta
+function mostrarAlerta() {
+  alert("Bem-vindo ao meu portfólio! Estou trabalhando duro para aprimorar minhas habilidades.");
+}
+
+// Função chamada quando a página carrega
+window.onload = mostrarAlerta;
